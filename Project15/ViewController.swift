14e98a3bfd6705d21ab19863a49fedc0ac937b5f
retessam7 <retessam7@gmail.com>
2025-01-23 02:31:43 +0100

    add some of switch/case, animate(withDuration:) method

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -19,7 +19,24 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
     }
 
-    @IBAction func tapped(_ sender: Any) {
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        // method accepts two closures. what animations to execute, and finally a closure that will execute when the animation finishes.
+        // don’t need to use [weak self] because there’s no risk of strong reference cycles here – the closures passed to animate(withDuration:) used once then thrown away
+        UIView.animate(withDuration: 1, delay: 0, options: [],
+                       animations: {
+            switch self.currentAnimation {
+            case 0:
+                break
+            
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        
         currentAnimation += 1
         
         if currentAnimation > 7 {
