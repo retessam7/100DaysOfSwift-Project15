5a634cbf49e0de83b805dbcdb69f2844ba1897f3
retessam7 <retessam7@gmail.com>
2025-01-23 02:59:42 +0100

    Transform: CGAffineTransform

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -24,12 +24,38 @@ class ViewController: UIViewController {
         
         // method accepts two closures. what animations to execute, and finally a closure that will execute when the animation finishes.
         // don’t need to use [weak self] because there’s no risk of strong reference cycles here – the closures passed to animate(withDuration:) used once then thrown away
-        UIView.animate(withDuration: 1, delay: 0, options: [],
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [],
                        animations: {
             switch self.currentAnimation {
             case 0:
-                break
+                // CGAffineTransform value that you can put into a view's transform property to apply it. Inside an animation block, transform automatically be animated
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
             
+            case 1:
+                self.imageView.transform = .identity
+                
+            case 2:
+                // values are deltas, or differences from the current value
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                
+            case 3:
+                self.imageView.transform = .identity
+                
+            case 4:
+                // Core Animation will always take the shortest route to make the rotation work!
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            
+            case 5:
+                self.imageView.transform = .identity
+                
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = UIColor.green
+                
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = UIColor.clear
+                
             default:
                 break
             }
@@ -46,3 +72,5 @@ class ViewController: UIViewController {
     
 }
 
+//  UIKit animations have an "ease in, ease out" curve, which means the movement starts slow, accelerates, then slows down again before reaching the end.
+// uses spring animations rather than the default, ease-in-ease-out animation
