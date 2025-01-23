c677f2da2682ead09caa762a06cdfae8449d4cd3
retessam <al@example.com>
2025-01-23 01:04:25 +0100

    Initial Commit

---
+++ Project15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project15
+//
+//  Created by VII on 23.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
